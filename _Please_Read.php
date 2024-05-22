45f1f49b5ff7062b132f264f9f5e9fb66bf4d558
njanel-msoi <n.janel@msoi.re>
2024-05-22 14:59:55 +0400

    test

--- _Please_Read.php
+++ _Please_Read.php
@@ -5,4 +5,5 @@
  * 
  * The templates files present in the root folder are here to override plugin & parent theme
  * Do not modify them directly and refer to templates files present in /templates folder.
+ * 
  */
