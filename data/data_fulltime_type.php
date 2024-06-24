5eb17cb23085587bc212b818ce44a87f81c39c72
njanel-msoi <n.janel@msoi.re>
2024-06-24 08:59:01 +0400

    fulltime type change

--- data/data_fulltime_type.php
+++ data/data_fulltime_type.php
@@ -4,7 +4,7 @@ $FULLTIME_TYPE = [
     "FULLTIME" => "Temps plein",
     "PARTTIME" => "Temps partiel"
 ];
-$FULLTIME_TYPE_MAP = str_arr_to_data_map($JOB_EXPERIENCE, true);
+$FULLTIME_TYPE_MAP = str_arr_to_data_map($FULLTIME_TYPE, true);
 
 
 function dtj_get_fulltime_type()
